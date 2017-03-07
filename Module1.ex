2cef356cfe04ab9a67c809428240e3a26148af61
Myrko Piche <mpiche@Myrkos-MBP.local>
2017-03-07 06:19:17 -0500

    init

---
+++ Module1.ex
@@ -0,0 +1,12 @@
+defmodule Module1 do
+
+	def fetchXML(url) do
+		
+	
+	end
+
+end
+
+url = "playerservices.streamtheworld.com/api/livestream?station=TRITONRADIOMUSIC&transports=http%2Chls%2Chlsts&version=1.9"
+
+fetchXML(url)
