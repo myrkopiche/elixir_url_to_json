b0c8c4d211b80d7e939afa6836e48d5e38e3e82f
Myrko Piche <mpiche@Myrkos-MBP.local>
2017-03-07 07:38:56 -0500

    fetch url

--- lib/xml_parse.ex
+++ lib/xml_parse.ex
@@ -12,7 +12,13 @@ defmodule XmlParse do
       :world
 
   """
-  def hello do
-    :world
+  def fetchXml() do
+    #{:ok, resp} = :httpc.request(:get, {url, []}, [], [body_format: :binary])
+    Response = :httpc.request(:get, {'http://playerservices.streamtheworld.com/api/livestream?station=TRITONRADIOMUSIC&transports=http%2Chls%2Chlsts&version=1.9', []}, [], [])
+    IO.puts(Response)
+    {:ok, "asdg"}
+    # {{_, 200, 'OK'}, _headers, body} = resp
+    # File.write!("/tmp/xml_tmp.xml", body)
+
   end
 end
--- test/xml_parse_test.exs
+++ test/xml_parse_test.exs
@@ -1,8 +1,14 @@
 defmodule XmlParseTest do
   use ExUnit.Case
   doctest XmlParse
+  
+  require Record
+  Record.defrecord :xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlText,    Record.extract(:xmlText,    from_lib: "xmerl/include/xmerl.hrl")
+  
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "parsing mountpoints" do
+    assert 1+1 == 2
   end
+
 end
