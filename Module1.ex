75490304740ac503ea991def73fc14630bd1cd2f
Myrko Piche <mpiche@Myrkos-MBP.local>
2017-03-07 06:40:12 -0500

    mix

--- Module1.ex
+++ Module1.ex
@@ -1,12 +1,14 @@
+require IEx;
+
 defmodule Module1 do
 
 	def fetchXML(url) do
+		IEx.pry
 		
-	
 	end
 
 end
 
 url = "playerservices.streamtheworld.com/api/livestream?station=TRITONRADIOMUSIC&transports=http%2Chls%2Chlsts&version=1.9"
 
-fetchXML(url)
+Module1.fetchXML(url)
---
+++ test/xml_parse_test.exs
@@ -0,0 +1,8 @@
+defmodule XmlParseTest do
+  use ExUnit.Case
+  doctest XmlParse
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
